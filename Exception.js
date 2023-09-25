9e5035023c3b94ac283d890200e1b7a1baf5da18
numan <keskin.nmn@gmail.com>
2023-09-25 12:48:54 +0300

    add examples for exception handling

---
+++ Exception.js
@@ -0,0 +1,30 @@
+function exception(param) {
+    if (param === undefined) {
+        throw new Error("param is undefined");
+    }
+    console.log(`param : ${param}`);
+}
+
+let param = undefined;
+
+try {
+    exception(param);
+} catch (error) {
+    console.log(error);
+} finally {
+    param = 1;
+}
+
+exception(param);
+
+class MyError extends Error{}
+
+function throwMyError() {
+    throw new MyError("My error is thrown");
+}
+
+try {
+    throwMyError();
+} catch (error) {
+    console.log(error)
+}
